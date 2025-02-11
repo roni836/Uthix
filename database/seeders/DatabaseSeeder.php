d1f05d97d093b7a8ac39dabd5f6dbbc31fbaed4e
SaritaKumari24 <saritaakumari24@gmail.com>
2025-02-11 22:18:46 +0530

    crud api of book

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
         $this->call(CategorySeeder::class);
+        $this->call(BookSeeder::class);
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,4 @@ Route::get('/test', function () {
 });
 
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('books', BookController::class);
