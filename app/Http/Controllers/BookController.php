8f9b25b0d8424200d9c6d6377cad39e0a9790e52
SaritaKumari24 <saritaakumari24@gmail.com>
2025-02-17 14:31:14 +0530

    small changes

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -132,6 +132,20 @@ class BookController extends Controller
 
 public function update(Request $request, $id)
 {
+    
+    $user = Auth::user();  
+
+    if ($user->role === 'seller' && $id->user_id !== $user->id) {
+        return response()->json([
+            'message' => 'You do not have permission to delete this book.',
+        ], 403);
+    }
+
+    if ($user->role !== 'admin' && $user->role !== 'seller') {
+        return response()->json([
+            'message' => 'You do not have permission to delete a book.',
+        ], 403);
+    }
     // Find the book or return error
     $book = Book::find($id);
     if (!$book) {
@@ -179,7 +193,7 @@ public function update(Request $request, $id)
         $book->title = $request->title ?? $book->title;
         $book->author = $request->author ?? $book->author;
         $book->category_id = $request->category_id ?? $book->category_id;
-        $book->user_id = $request->user_id ?? $book->user_id;
+        $book->user_id = $request->$user->id ?? $book->$user->id;
         $book->isbn = $request->isbn ?? $book->isbn;
         $book->language = $request->language ?? $book->language;
         $book->pages = $request->pages ?? $book->pages;
@@ -216,6 +230,19 @@ public function update(Request $request, $id)
      */
     public function destroy(Book $book)
     {
+            $user = Auth::user();
+
+        if ($user->role === 'seller' && $book->user_id !== $user->id) {
+            return response()->json([
+                'message' => 'You do not have permission to delete this book.',
+            ], 403);
+        }
+    
+        if ($user->role !== 'admin' && $user->role !== 'seller') {
+            return response()->json([
+                'message' => 'You do not have permission to delete a book.',
+            ], 403);
+        }
         try {
             $book->delete();
             return response()->json(['message' => 'Book deleted successfully'], 200);
