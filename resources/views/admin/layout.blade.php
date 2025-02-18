312b8a0bc9bd97a0f8dcc708846b359b73752e9f
roni836 <ronitsaha836@gmail.com>
2025-02-18 20:55:06 +0530

    admin panel | product insertion done

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -234,12 +234,12 @@
                                 </a>
                                 <ul class="menu-sub">
                                     <li class="menu-item">
-                                        <a href="app-ecommerce-product-list.html" class="menu-link">
+                                        <a href="{{route('manage.product')}}" class="menu-link">
                                             <div data-i18n="Product List">Product List</div>
                                         </a>
                                     </li>
                                     <li class="menu-item">
-                                        <a href="app-ecommerce-product-add.html" class="menu-link">
+                                        <a href="{{route('insert.product')}}" class="menu-link">
                                             <div data-i18n="Add Product">Add Product</div>
                                         </a>
                                     </li>
