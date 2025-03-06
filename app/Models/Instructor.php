9a7415885a38f7c4297731a8d264bb72a18b5247
roni836 <ronitsaha836@gmail.com>
2025-03-06 08:09:32 +0530

    import fixed

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -1,21 +1,21 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Instructor extends Model
 {
     protected $guarded=[];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
-
     public function classrooms()
     {
         return $this->hasMany(Classroom::class, 'instructor_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
     
 }
