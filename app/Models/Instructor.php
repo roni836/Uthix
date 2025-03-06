781ee471a9e00cb273666ee533c174450e78e028
roni836 <ronitsaha836@gmail.com>
2025-03-06 08:03:56 +0530

    error fixing

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -10,7 +10,7 @@ class Instructor extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function classrooms()
