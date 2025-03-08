f7d77d34134c7a4b837ebb51e373f806b411ed61
roni836 <ronitsaha836@gmail.com>
2025-03-08 14:09:45 +0530

    some data added

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Artisan;
 
 // Route::get('/', function () {
@@ -13,31 +14,32 @@ use Illuminate\Support\Facades\Artisan;
 
 
 Route::get('/login', function () {
-    return view('auth.login');  
+    return view('auth.login');
 })->name('login');
 
 // Route::get('/', [AdminController::class, 'dashboard'])->name('dashboard'); 
 
- 
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::middleware([RoleMiddleware::class . ':admin'])->group(function () {
 
-   
-    Route::get('/', function () {
-        return view('admin.index');  
-    })->name('admin.dashboard');
-    Route::get('/manage-student', [AdminController::class, 'manageStudent'])->name('manage.student'); 
-    Route::get('/insert-student', [AdminController::class, 'insertStudent'])->name('insert.student'); 
-    Route::get('/insert-vendor', [AdminController::class, 'insertVendor'])->name('insert.vendor'); 
-    Route::get('/manage-vendor', [AdminController::class, 'manageVendor'])->name('manage.vendor'); 
-    Route::get('/insert-instructor', [AdminController::class, 'insertInstructor'])->name('insert.instructor'); 
-    Route::get('/manage-instructor', [AdminController::class, 'manageInstructor'])->name('manage.instructor'); 
-    Route::get('/manage-category', [AdminController::class, 'manageCategory'])->name('manage.category'); 
-    Route::get('/insert-product', [AdminController::class, 'insertProduct'])->name('insert.product'); 
-    Route::get('/manage-product', [AdminController::class, 'manageProduct'])->name('manage.product'); 
-    Route::get('/manage-coupon', [AdminController::class, 'manageCoupon'])->name('manage.coupon'); 
-    Route::get('/insert-coupon', [AdminController::class, 'insertCoupon'])->name('insert.coupon'); 
-});
+        Route::get('/', function () {
+            return view('admin.index');
+        })->name('admin.dashboard');
+        Route::get('/manage-student', [AdminController::class, 'manageStudent'])->name('manage.student');
+        Route::get('/insert-student', [AdminController::class, 'insertStudent'])->name('insert.student');
+        Route::get('/insert-vendor', [AdminController::class, 'insertVendor'])->name('insert.vendor');
+        Route::get('/manage-vendor', [AdminController::class, 'manageVendor'])->name('manage.vendor');
+        Route::get('/insert-instructor', [AdminController::class, 'insertInstructor'])->name('insert.instructor');
+        Route::get('/manage-instructor', [AdminController::class, 'manageInstructor'])->name('manage.instructor');
+        Route::get('/manage-category', [AdminController::class, 'manageCategory'])->name('manage.category');
+        Route::get('/insert-product', [AdminController::class, 'insertProduct'])->name('insert.product');
+        Route::get('/manage-product', [AdminController::class, 'manageProduct'])->name('manage.product');
+        Route::get('/manage-coupon', [AdminController::class, 'manageCoupon'])->name('manage.coupon');
+        Route::get('/insert-coupon', [AdminController::class, 'insertCoupon'])->name('insert.coupon');
+        Route::get('/all-orders', [OrderController::class, 'allOrders'])->name('orders.all');
+
+    });
 });
 Route::post('/admin-login', [AuthController::class, 'adminLogin'])->name('admin.login');
 
@@ -51,5 +53,3 @@ Route::get('/clear-cache', function () {
 
     return "All Caches are cleared by @Roni";
 });
-
-
