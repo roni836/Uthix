f4429534047e6325b6a011da65b11ab9fa3dcdcc
roni836 <ronitsaha836@gmail.com>
2025-03-20 16:34:31 +0530

    fcm data updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -129,6 +129,7 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
+            'fcm_token' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
             'role' => 'required|string|in:student,instructor,seller,admin',
@@ -146,7 +147,7 @@ class AuthController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'role' => $role,
-            'fcm_token' => Str::random(64), 
+            'fcm_token' => $request->fcm_token, 
         ]);
 
         // Generate authentication token
