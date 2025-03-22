8f32e8940e268142e8ed4dc216e71246996d88d9
SaritaKumari24 <saritaakumari24@gmail.com>
2025-03-22 15:55:40 +0530

    fixed admin panel

--- resources/views/admin/manageVendor.blade.php
+++ resources/views/admin/manageVendor.blade.php
@@ -2,6 +2,17 @@
 @section('title', 'Manage Users')
 @section('content')
 
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
 
                     <div class="container-xxl flex-grow-1 container-p-y">
 
@@ -69,6 +80,55 @@
 
 
                     </div>
-                   
+                    <!-- / Content -->
+
+                    <!-- Footer -->
+                    <footer class="content-footer footer bg-footer-theme">
+                        <div class="container-xxl">
+                            <div
+                                class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+                                <div class="text-body">
+                                    ©
+                                    <script>
+                                        document.write(new Date().getFullYear());
+                                    </script>
+                                    , made with ❤️ by <a href="https://trapigo.in/" target="_blank"
+                                        class="footer-link">Trapigo</a>
+                                </div>
+                                <div class="d-none d-lg-inline-block">
+                                    <a href="https://themeforest.net/licenses/standard" class="footer-link me-4"
+                                        target="_blank">License</a>
+                                    <a href="https://1.envato.market/pixinvent_portfolio" target="_blank"
+                                        class="footer-link me-4">More Themes</a>
+
+                                    <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/"
+                                        target="_blank" class="footer-link me-4">Documentation</a>
+
+                                    <a href="https://pixinvent.ticksy.com/" target="_blank"
+                                        class="footer-link d-none d-sm-inline-block">Support</a>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+                    <!-- / Footer -->
+
+                    <div class="content-backdrop fade"></div>
+                </div>
+                <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+
+        <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+        <div class="drag-target"></div>
+    </div>
+
+
+    <!-- / Navbar -->
+
+    <!-- Content wrapper -->
 
 @endsection
