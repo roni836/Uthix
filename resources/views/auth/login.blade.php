f21ef5085bcef7a8c25994914fae65cd74b044ea
SaritaKumari24 <saritaakumari24@gmail.com>
2025-09-01 14:18:34 +0530

    fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>@yield('title') - {{ config('app.name') }}</title>
+    <title>Login - {{ config('app.name') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="description" content="" />
