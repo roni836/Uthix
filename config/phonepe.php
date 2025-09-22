4be2d8cf591fcc15f351160bec339f9507df91ad
roni836 <ronitsaha836@gmail.com>
2025-09-22 14:35:01 +0530

    code rem

--- config/phonepe.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'merchant_id' => env('PHONEPE_MERCHANT_ID', 'your_merchant_id'),
-    'salt_key' => env('PHONEPE_SALT_KEY', 'your_salt_key'),
-    'salt_index' => env('PHONEPE_SALT_INDEX', 'your_salt_index'),
-    'environment' => env('PHONEPE_ENV', 'UAT'), // UAT for testing, PRODUCTION for live
-];
